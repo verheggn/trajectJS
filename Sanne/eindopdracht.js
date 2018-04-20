aed5f6b1e894e1a5eaab0393fdb8e27e9a87be6d
Jasper Verheggen <jasper.verheggen@ogd.nl>
2018-04-20 09:28:29 +0200

    meer eindopdracht

--- Sanne/eindopdracht.js
+++ Sanne/eindopdracht.js
@@ -1,56 +1,109 @@
-//opdracht 1: maak een functie om de volledige URL op te bouwen
-// goede antwoord:
+// Assignment 1 - Create an API-key on openweathermaps.com using a browser, and store this in a variable.
+// Correct answer:
+let apiKey = "c7d6b1959579335813b946da059d652b";
+
+//Assignment 2 - Figure out how the OpenWeatherMaps API-works, then build a function which takes two arguements: city and apiKey.
+//The function will then return the full URL.
+//Verify that the URL is working through your browser. The outcome will be an object formatted in JSON.
+//Correct answer:
 let urlBuilder = function (city, apiKey) {
-	let urlBase = "http://api.openweathermap.org/data/2.5/weather?q=";
+    const urlBase = "http://api.openweathermap.org/data/2.5/weather?q=";
     return urlBase + city + "&APPID=" + apiKey;
-}
+};
+// verify it's working by calling the function and logging the output to the console.
+// correct answer
+var urlAmsterdam = urlBuilder("Amsterdam", apiKey);
+console.log(urlAmsterdam);
 
-//opdracht 2: maak de async await fuctie af op de API aan te roepen (invulopdracht)
+// Assignment 3 - Finish the function below to create an async await function. The purpose of this function is to make sure the API call waits till the Javascript page has loaded as well.
 //[invullen] [invullen] apiCall(url) {
-  //const response = [invullen] fetch(url);
-  //const json = [innvullen](response.json())
-
-    //return json;
+//const response = [invullen] [invullen](url);
+//const json = [innvullen]([invullen].json())
+//return json;
 //}
 
-//goede antwoord:
+//Correct answer:
 async function apiCall(url) {
   const response = await fetch(url);
   const json = await(response.json());
-
-    return json;
+  return json;
 }
 
-//opdracht 3: open de URL om te kijken wat de exacte output is en maak een functie om de temp_min en temp_max uit de API te trekken
+// Assignment 4 - open de URL to see what the exacte output is and create a function to retrieve the temp_min and temp_max from the API.
+// Tip 1: Use the function from assignment 2
+// Tip 2: Use the console to figure it out, or enter the full URL in your browser to see the structure of the API-reply.
+// Correct answer:
 async function apiCall(url) {
   const response = await fetch(url);
   const json = await(response.json());
-
-    return json.main.temp_min;
-	return json.main.temp_max;
+  let temp_min = json.main.temp_min;
+  let temp_max = json.main.temp_max;
+  return temp_min, temp_max;
+  console.log("temp_min 2= ", temp_min, "temp_max 2= ", temp_max);
+  //return temp_min, temp_max;
 }
 
-//opdracht 4: zet de uitkomst van opdracht 3 om naar een float
-// goede antwoord:
-let tempMinFloat = float(temp_min);
-let tempMaxFloat = float(temp_max);
 
-//opdracht 5: zet de kelvin temp om in celsius temp
-//goede antwoord:
-let tempMinK = tempMinFloat;
-let tempMaxK = tempMaxFloat;
 
-function KelvinToCelsius(temp){
-	let tempMinC= tempMinK  - 273.15;
-	let tempMaxC = tempMaxK - 273.15;
-	return tempMinC, tempMaxC;
-}
+var x = temp_min_function(urlAmsterdam).then(function(value){
+    console.log("value = ", value);
+    console.log("temp_min 1 = ", temp_min);
+});
 
-//opdracht 6: bereken met temp_min en temp_max de gemiddelde temperatuur
-let averageTemp = (tempMinC+ tempMaxC)/2;
-console.log(averageTemp);
+/*
+//Assignment 5 - the temparatures you just retrieved are strings. Find out how you can convert them into floats.
+// Correct answer:
+let tempMinFloat = parseFloat(temp_min);
+let tempMaxFloat = parseFloat(temp_max);
+
+//Assignment 6 - As you might have noticed it's pretty hot in Amsterdam. Maybe the API is providing us the temparture in the wrong metric system?
+//Find out how you can convert the temperatures from Kelvin to Celsius using an arrow function.
+//Correct answer:
+//let KelvinToCelsius = (tempMinFloat,tempMaxFloat) => {
+//    let tempMinC= tempMinFloat  - 273.15;
+//    let tempMaxC = tempMaxFloat - 273.15;
+//    return tempMinC, tempMaxC;
+//}
+
+//Assignment 7 - Store the average temperature in Celcius in a variable and log it to the console
+//Correct answer:
+//let tempAverage = (tempMinC + tempMaxC)/2;
+//console.log(tempAverage);
+
 
 /*eventueel nog extra:
 - Trek de coördinaten uit de API
 - Gebruik Google Maps API om een kaartje te plotten
 - Zet een marker op de juiste stad */
+
+
+/*
+
+//coördinate uit API trekken
+async function apiCall(url) {
+    const response = await fetch(url);
+    const json = await(response.json());
+    let lat = json.coord.lat;
+    let lng = json.coord.lon;
+    return lat, lng;
+    console.log("lat = ", lat, "lng =", lng);
+
+}
+
+
+var map;
+
+//open a Google Maps//
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {
+            //display Amsterdam//
+            lat: 52.370216, lng: 4.895168
+        },
+        //zoom in on Amsterdam//
+        zoom: 12
+
+    });
+}
+
+*/
